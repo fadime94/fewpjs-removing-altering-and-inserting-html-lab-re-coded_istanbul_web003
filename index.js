945230e469c9e420dce794c60bb12f14831b5774
fadime94 <fadime.ozdemir11@gmail.com>
2020-07-19 19:41:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let element = document.querySelector('#main');
+element.remove();
+
+let newHeader  = document.createElement("h1");
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML= "it is the champion";
+
+
